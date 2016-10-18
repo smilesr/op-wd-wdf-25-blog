689b65731380c6f7f5be14cbb525dbae9e39a9e3
Robert Smiles Jr <smilesr120@gmail.com>
2016-10-18 09:34:52 -0400

    initial commit; tutorial

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
